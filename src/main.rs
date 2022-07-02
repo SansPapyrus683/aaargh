51ef9aa140ea4378fc99c046a89d5d17a2bfa3a2
Shubham Patil <shubhampatilsd@gmail.com>
2022-07-01 18:26:18 -0700

    cli args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    // #[structopt(short = "v", long = "verbose", help = "Print verbose output")]
+    // verbose: bool,
+    #[structopt()]
+    input_test_file: String,
+    #[structopt()]
+    output_test_file: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+    println!("aaargh {}", args.input_test_file);
 }
